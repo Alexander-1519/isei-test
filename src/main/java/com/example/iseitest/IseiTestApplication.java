bd07f0fc4065d89421959ea1da66896d79afdad8
alexander.rybak <alexander.rybak2020@mail.ru>
2022-03-25 10:40:18 +0300

    Initial commit

---
+++ src/main/java/com/example/iseitest/IseiTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.iseitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IseiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IseiTestApplication.class, args);
+    }
+
+}
