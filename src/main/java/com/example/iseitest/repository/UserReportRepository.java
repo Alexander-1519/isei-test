65cf4f0bd860bcd9905ba80a58f8ab2dacf94ad5
alexander.rybak <alexander.rybak2020@mail.ru>
2022-04-07 12:05:51 +0300

    update db queries in reportService

--- src/main/java/com/example/iseitest/repository/UserReportRepository.java
+++ src/main/java/com/example/iseitest/repository/UserReportRepository.java
@@ -3,6 +3,7 @@ package com.example.iseitest.repository;
 import com.example.iseitest.entity.UserReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,11 +12,11 @@ import java.util.List;
 public interface UserReportRepository extends JpaRepository<UserReport, Long> {
 
     @Query("FROM UserReport ur WHERE ur.user.id = :id")
-    List<UserReport> findByUserId(Long id);
+    List<UserReport> findByUserId(@Param("id") Long id);
 
     @Query("FROM UserReport ur WHERE ur.belongCompany = false")
     List<UserReport> findByBelongCompanyIsFalse();
 
     @Query("FROM UserReport ur WHERE ur.belongCompany = true AND ur.user.company.name = :companyName")
-    List<UserReport> findByCompanyName(String companyName);
+    List<UserReport> findByCompanyName(@Param("companyName") String companyName);
 }
