c613c2823da42f5408bd46db0ab4e245d1b1b54f
alexander.rybak <alexander.rybak2020@mail.ru>
2022-04-20 17:33:10 +0300

    add search by name

--- src/main/java/com/example/iseitest/repository/CompanyTagRepository.java
+++ src/main/java/com/example/iseitest/repository/CompanyTagRepository.java
@@ -3,6 +3,7 @@ package com.example.iseitest.repository;
 import com.example.iseitest.entity.CompanyTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +12,5 @@ import java.util.List;
 public interface CompanyTagRepository extends JpaRepository<CompanyTag, Long> {
 
     @Query("SELECT t FROM CompanyTag t WHERE t.name LIKE concat('%', upper(:name) , '%')")
-    List<CompanyTag> getAllWithFiltering(String name);
+    List<CompanyTag> getAllWithFiltering(@Param("name") String name);
 }
