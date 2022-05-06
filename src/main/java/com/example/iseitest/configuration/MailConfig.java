01d5f6fdd8e44539124af172ee5a08330fda59af
alexander.rybak <alexander.rybak2020@mail.ru>
2022-05-06 14:25:57 +0300

    change mail auth

--- src/main/java/com/example/iseitest/configuration/MailConfig.java
+++ src/main/java/com/example/iseitest/configuration/MailConfig.java
@@ -1,27 +1,27 @@
-package com.example.iseitest.configuration;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-
-import java.util.Properties;
-
-@Configuration
-public class MailConfig {
-
-    @Bean
-    public JavaMailSender getMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setUsername("alexander.rybak2021@gmail.com");
-        mailSender.setPassword("Lera6529963");
-        mailSender.setPort(465);
-
-        Properties properties = mailSender.getJavaMailProperties();
-        properties.setProperty("mail.transport.protocol", "smtps");
-
-        return mailSender;
-    }
-}
+//package com.example.iseitest.configuration;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.mail.javamail.JavaMailSenderImpl;
+//
+//import java.util.Properties;
+//
+//@Configuration
+//public class MailConfig {
+//
+//    @Bean
+//    public JavaMailSender getMailSender() {
+//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+//
+//        mailSender.setHost("smtp.gmail.com");
+//        mailSender.setUsername("alexander.rybak2021@gmail.com");
+//        mailSender.setPassword("Lera6529963");
+//        mailSender.setPort(465);
+//
+//        Properties properties = mailSender.getJavaMailProperties();
+//        properties.setProperty("mail.transport.protocol", "smtps");
+//
+//        return mailSender;
+//    }
+//}
